12335b1e8fd72c08fbb3d1a42a1a700015076192
Hugo-Diniz <hugodiniztech01@gmail.com>
2024-10-07 21:09:46 -0300

    feat: Criando a classe principal para o controler das exceções

---
+++ src/main/java/br/edu/ifpb/projeto/estacaoMetereologica/controllers/exceptions/ControllerExceptionHandler.java
@@ -0,0 +1,32 @@
+package br.edu.ifpb.projeto.estacaoMetereologica.controllers.exceptions;
+
+import br.edu.ifpb.projeto.estacaoMetereologica.services.exceptions.StationNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import java.time.Instant;
+
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(StationNotFoundException.class)
+    public ResponseEntity<StandardError> stationNotFoundException(StationNotFoundException e, HttpServletRequest request) {
+        String error = "Station not found.";
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<StandardError> dateInvalidException(MethodArgumentTypeMismatchException e, HttpServletRequest request) {
+        String error = "Date invalid.";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+}
